7315100b404ff7a28b6c5ef2a20679e06e0f6aa2
seonhyeokjun <tjsrmatn@naver.com>
2022-02-14 22:49:50 +0900

    Initial commit

---
+++ src/main/java/com/codigtest/CodigTestApplication.java
@@ -0,0 +1,13 @@
+package com.codigtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodigTestApplication.class, args);
+    }
+
+}
