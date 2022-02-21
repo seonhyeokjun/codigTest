94c7ba18b7f9d745e64812145ba68524fb9121d8
seonhyeokjun <tjsrmatn@naver.com>
2022-02-21 22:15:01 +0900

    두 포인터

--- src/main/java/com/codigtest/twopointer/BOJ1806.java
+++ src/main/java/com/codigtest/twopointer/BOJ1806.java
@@ -1,55 +1,49 @@
-package com.codigtest.sort;
+package com.codigtest.twopointer;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Sort2 {
-    static FastReader scan = new FastReader();
+public class BOJ1806 {
     static StringBuilder sb = new StringBuilder();
+    static FastReader scan = new FastReader();
 
-    static class Elem implements Comparable<Elem> {
-
-        public int num, idx;
-
-        @Override
-        public int compareTo(Elem other) {
-            return num - other.num;
-        }
-    }
-
-    static int N;
-    static int[] P;
-    static Elem[] B;
+    static int n, S;
+    static int[] a;
 
     static void input() {
-        N = scan.nextInt();
-        B = new Elem[N];
-        P = new int[N];
-        for (int i = 0; i < N; i++) {
-            B[i] = new Elem();
-            B[i].num = scan.nextInt();
-            B[i].idx = i;
+        n = scan.nextInt();
+        S = scan.nextInt();
+        a = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            a[i] = scan.nextInt();
         }
     }
 
     static void pro() {
-        Arrays.sort(B);
+        int R = 0, sum = 0, ans = n + 1;
+        for (int L = 1; L <= n; L++) {
+            // L - 1 을 구간에서 제외하기
+            sum -= a[L - 1];
 
-        for (int i = 0; i < N; i++) {
-            P[B[i].idx] = i;
+            // R 을 옮길 수 있을 때 까지 옮기기
+            while (R + 1 <= n && sum < S)
+                sum += a[++R];
+
+            // [L ... R] 의 합, 즉 sum이 조건을 만족하면 정답 갱신하기
+            if (sum >= S)
+                ans = Math.min(ans, R - L + 1);
         }
-        for (int i = 0; i < N; i++) {
-            sb.append(P[i]).append(' ');
-        }
-        System.out.println(sb.toString());
+
+        // ans 값을 보고 불가능 판단하기
+        if (ans == n + 1)
+            ans = 0;
+        System.out.println(ans);
     }
 
     public static void main(String[] args) {
         input();
         pro();
     }
-
 
     static class FastReader {
         BufferedReader br;
