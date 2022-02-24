26cb72b0174fe7ab609654487ebe20070acb2b62
seonhyeokjun <tjsrmatn@naver.com>
2022-02-24 21:34:09 +0900

    투 포인트 - 응용

---
+++ src/main/java/com/codigtest/twopointer/BOJ16472.java
@@ -0,0 +1,107 @@
+package com.codigtest.twopointer;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ16472 {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, kind;
+    static String A;
+    static int[] cnt;
+
+    static void input() {
+        N = scan.nextInt();
+        A = scan.nextLine();
+        cnt = new int[26];
+    }
+
+    static void add(char x) {  // x 라는 알파벳 추가
+        /* TODO */
+        cnt[x-'a']++;
+        if (cnt[x - 'a'] == 1) {
+            kind++;
+        }
+    }
+
+    static void erase(char x) {  // x 라는 알파벳 제거
+        /* TODO */
+        cnt[x-'a']--;
+        if (cnt[x - 'a'] == 0) {
+            kind--;
+        }
+    }
+
+    static void pro() {
+        int len = A.length(), ans = 0;
+        for (int R = 0, L = 0; R < len; R++) {
+            // R 번째 문자를 오른쪽에 추가
+            /* TODO */
+            add(A.charAt(R));
+
+            // 불가능하면, 가능할 때까지 L을 이동
+            /* TODO */
+            while (kind > N) {
+                erase(A.charAt(L++));
+            }
+
+            // 정답 갱신
+            /* TODO */
+            ans = Math.max(ans, R - L + 1);
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
