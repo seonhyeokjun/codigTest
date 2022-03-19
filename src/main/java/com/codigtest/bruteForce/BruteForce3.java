7038a13d21f27c7093843cd8e5092c362194bd50
seonhyeokjun <tjsrmatn@naver.com>
2022-03-19 22:30:03 +0900

    BOJ 1260번 문제

--- src/main/java/com/codigtest/bruteForce/BruteForce3.java
+++ src/main/java/com/codigtest/bruteForce/BruteForce3.java
@@ -1,51 +1,84 @@
 package com.codigtest.bruteForce;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BruteForce3 {
+    static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
+    static int N, M, V;
+    static ArrayList<Integer>[] adj;
+    static boolean[] visit;
+
     static void input() {
-        FastReader scan = new FastReader();
         N = scan.nextInt();
         M = scan.nextInt();
-        selected = new int[M + 1];
+        V = scan.nextInt();
+        adj = new ArrayList[N + 1];
+        for (int i = 1;i <= N; i++)
+            adj[i] = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            int x = scan.nextInt(), y = scan.nextInt();
+            adj[x].add(y);
+            adj[y].add(x);
+        }
+        for (int i = 1;i<=N;i++)
+            Collections.sort(adj[i]);
     }
 
-    static int N, M;
-    static int[] selected;
+    // x 를 갈 수 있다는 걸 알고 방문한 상태
+    static void dfs(int x) {
+        // x 를 방문했다.
+        visit[x] = true;
+        sb.append(x).append(' ');
 
-    // Recurrence Function (재귀 함수)
-    // 만약 M 개를 전부 고름   => 조건에 맞는 탐색을 한 가지 성공한 것!
-    // 아직 M 개를 고르지 않음 => k 번째부터 M번째 원소를 조건에 맞게 고르는 모든 방법을 시도한다.
-    static void rec_func(int k) {
-        if (k == M + 1) { // 다 골랐다!
-            // selected[1...M] 배열이 새롭게 탐색된 결과
-            for (int i = 1; i <= M; i++) {
-                sb.append(selected[i]).append(' ');
-            }
-            sb.append('\n');
-        } else {
-            int start = selected[k-1];
-            if (start == 0) start = 1;
-            for (int cand = start; cand <= N; cand++) {
-                // k 번째에 cand 가 올 수 있으면
-                selected[k] = cand;
+        // x 에서 갈 수 있는 곳들을 작은 번호부터 모두 방문한다.
+        for (int y: adj[x]){
 
-                // k+1 번부터 M 번까지 잘 채워주는 함수를 호출해준다.
-                rec_func(k + 1);
-                selected[k] = 0;
+            // y 를 이미 갈 수 있다는 사실을 안다면, 굳이 갈 필요 없다.
+            if (visit[y])
+                continue;
+
+            // y에서 갈 수 있는 곳들도 확인 해보자
+            dfs(y);
+        }
+    }
+
+    // start 에서 시작해서 갈 수 있는 정점들을 모두 탐색하기
+    static void bfs(int start) {
+        Queue<Integer> que = new LinkedList<>();
+
+        // start는 방문 가능한 점이므로 que에 넣어준다.
+        que.add(start);
+        visit[start] = true;  // start를 갈 수 있다고 표시하기 (중요!!!)
+
+        while (!que.isEmpty()) {  // 더 확인할 점이 없다면 정지
+            int x = que.poll();
+
+            sb.append(x).append(' ');
+            for (int y: adj[x]){
+                if (visit[y]) continue;  // x 에서 y 를 갈 수는 있지만, 이미 탐색한 점이면 무시
+
+                // y를 갈 수 있으니까 que에 추가하고, visit 처리 하기!
+                que.add(y);
+                visit[y] = true;
             }
         }
     }
 
+    static void pro() {
+        visit = new boolean[N + 1];
+        dfs(V);
+        sb.append('\n');
+        for (int i = 1; i <= N; i++) visit[i] = false;
+        bfs(V);
+        System.out.println(sb);
+    }
+
     public static void main(String[] args) {
         input();
-
-        // 1 번째 원소부터 M 번째 원소를 조건에 맞는 모든 방법을 찾아줘
-        rec_func(1);
-        System.out.println(sb.toString());
+        pro();
     }
 
 
